2401d185a8850b2fd8fbed255ff009cfc5fa585c
Yasir Khan <Yasirkahn6499@gmail.com>
2024-02-22 06:22:43 +0000

    removed the loading from edit page!

--- app/books/additem/[id]/page.jsx
+++ app/books/additem/[id]/page.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 import { useEffect, useState } from "react"
 import { useRouter } from 'next/navigation'; // Importing useRouter
 import { useToast } from "@/context/ToastContext";
-import {ColorRing, Loader} from 'react-loader-spinner';
+
 const page = ({params}) => {
 
   const router = useRouter(); // Initializing useRouter
@@ -33,13 +33,11 @@ const page = ({params}) => {
 
   const [book, setBook] = useState();
   const [collection, setCollection] = useState();
-  const [isLoading, setIsLoading] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
 
   // taost
   const { triggerToast } = useToast();
   useEffect( ()=>{
-    setIsLoading(true);
-
     try{
       // get the book
     const getBookData = async ()=>{
@@ -48,7 +46,6 @@ const page = ({params}) => {
     });
     // alert("book data")
     console.log("bookData :",response.data.bookData);
-    if(response.data.success){
     setBook(response.data.bookData);
 
     // get the collection of the book
@@ -58,35 +55,21 @@ const page = ({params}) => {
     console.log("Single Book Collection:", response2.data.collection);
 
     setCollection(response2.data.collection);
-  }else{
-    setIsLoading(false);
-  }
   }
   getBookData();
 }catch(error){
-  triggerToast("Couldn't Fetch The Book. Try Refreshing The Page!")
+  triggerToast("Couldn't Fetch The Book. Try Refreshing The Page","error");
   console.log(error);
-} finally{
-  setIsLoading(false);
 }
   },[])
 
   return (
     <div>
-      {isLoading?<ColorRing
-            visible={true}
-            height="150"
-            width="150"
-            ariaLabel="color-ring-loading"
-            wrapperStyle={{}}
-            wrapperClass="color-ring-wrapper"
-            colors={['#22d3ee', '#22d3ee', '#22d3ee', '#22d3ee', '#22d3ee']}
-          />
-        :book?<BookEdit 
-          book={book}
-          collection={collection}
-          cancelBtnUrl={returnUrl?returnUrl:"/books/additem"}
-          />:""}
+      {book?<BookEdit 
+      book={book}
+      collection={collection}
+      cancelBtnUrl={returnUrl?returnUrl:"/books/additem"}
+      />:""}
       
     </div>
   )
